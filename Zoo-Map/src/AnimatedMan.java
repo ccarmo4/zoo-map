5f4d0823e80df645ed4f8e2e686035037f28dde2
brenda <brenda.mtz22@yahoo.com>
2015-03-26 15:51:09 -0500

    Animated Man's Class

---
+++ Zoo-Map/src/AnimatedMan.java
@@ -0,0 +1,22 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+
+public class AnimatedMan {
+
+	
+	public static void drawMan(Graphics g)
+	{
+		//Animated Man
+
+		g.setColor(new Color(244, 206, 149));
+		g.fillOval(20, 550, 80, 80);
+		g.fillRect(48, 625, 25, 10);
+		g.setColor(new Color(65, 20, 255));
+		g.fillRect(40, 635, 40, 65);
+		g.setColor(Color.BLACK);
+		g.fillOval(40, 575, 10, 10);//eyes
+		g.fillOval(60, 575, 10, 10);
+		
+	}
+}
--- Zoo-Map/src/Map.java
+++ Zoo-Map/src/Map.java
@@ -66,17 +66,7 @@ serialVersionUID = 1L;
 		g.setFont(new Font("JOE'S BURGER SHACK", Font.ITALIC, 12));
 		g.drawString("JOE'S BURGER SHACK", 605, 225);
 		
-	//Animated Man
-
-		g.setColor(new Color(244, 206, 149));
-		g.fillOval(20, 550, 80, 80);
-		g.fillRect(48, 625, 25, 10);
-		g.setColor(new Color(65, 20, 255));
-		g.fillRect(40, 635, 40, 65);
-		g.setColor(Color.BLACK);
-		g.fillOval(40, 575, 10, 10);//eyes
-		g.fillOval(60, 575, 10, 10);
-		
+		AnimatedMan.drawMan(g);
 		
 		
 		
