b62e209b40d5d000a17e643179a62abf0232726d
brenda <brenda.mtz22@yahoo.com>
2015-03-26 15:46:11 -0500

    moved stuff

--- Zoo-Map/src/Map.java
+++ Zoo-Map/src/Map.java
@@ -31,40 +31,40 @@ serialVersionUID = 1L;
 		
 		//Restrooms
 		g.setColor(new Color(128, 128, 128)); //building
-		g.fillRect(800, 100, 220, 100);
+		g.fillRect(1200, 100, 220, 100);
 		g.setColor(new Color(145, 145, 145)); //divider
-		g.drawRect(800, 110, 110, 90);		
-		g.drawRect(910, 110, 110, 90);
+		g.drawRect(1200, 110, 110, 90);		
+		g.drawRect(1310, 110, 110, 90);
 		g.setColor(Color.blue); //doors
-		g.fillRect(820, 165, 20, 35);		
-		g.fillRect(980, 165, 20, 35);
+		g.fillRect(1220, 165, 20, 35);		
+		g.fillRect(1380, 165, 20, 35);
 		g.setColor(Color.white); //signs
 		g.setFont(new Font("RESTROOMS", Font.ITALIC, 12));
-		g.drawString("RESTROOMS", 870, 110);
+		g.drawString("RESTROOMS", 1270, 110);
 		g.setFont(new Font("MENS", Font.BOLD, 10));
-		g.drawString("MENS", 817, 160);		
-		g.drawString("WOMENS", 967, 160);
+		g.drawString("MENS", 1217, 160);		
+		g.drawString("WOMENS", 1367, 160);
 		
 		//Gift Shop
 		g.setColor(new Color(128, 64, 64)); //building
-		g.fillRect(200, 300, 140, 140); 
+		g.fillRect(200, 250, 140, 140); 
 		g.setColor(Color.gray); //doors
-		g.drawRect(247, 405, 20, 35); 
-		g.drawRect(246, 404, 22, 37);
-		g.drawRect(245, 403, 23, 38);
-		g.drawRect(267, 405, 20, 35);
-		g.drawRect(266, 404, 22, 37);
-		g.drawRect(265, 403, 24, 38);
+		g.drawRect(247, 355, 20, 35); 
+		g.drawRect(246, 354, 22, 37);
+		g.drawRect(245, 353, 23, 38);
+		g.drawRect(267, 355, 20, 35);
+		g.drawRect(266, 354, 22, 37);
+		g.drawRect(265, 353, 24, 38);
 		g.setColor(Color.white); //sign
 		g.setFont(new Font("GIFT SHOP", Font.ITALIC, 14));
-		g.drawString("GIFT SHOP", 235, 325);
+		g.drawString("GIFT SHOP", 235, 275);
 		
 		//Restaurant (Burger place)
 		g.setColor(Color.yellow);
-		g.fillRect(500, 200, 140, 140); //building
+		g.fillRect(600, 200, 140, 140); //building
 		g.setColor(Color.black);
 		g.setFont(new Font("JOE'S BURGER SHACK", Font.ITALIC, 12));
-		g.drawString("JOE'S BURGER SHACK", 505, 225);
+		g.drawString("JOE'S BURGER SHACK", 605, 225);
 		
 		//Animated Man
 		g.setColor(new Color(244, 206, 149));
