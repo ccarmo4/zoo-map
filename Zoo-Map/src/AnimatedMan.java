810fa061ec26a849d81185447f38b3977afe8bce
ccarmona9800 <ccarmona9800@001LAB102010551.neisd.ad>
2015-03-27 15:37:12 -0500

    taking away stuff

--- Zoo-Map/src/AnimatedMan.java
+++ Zoo-Map/src/AnimatedMan.java
@@ -11,17 +11,16 @@ public class AnimatedMan {
 
 		// same x, just add to change....same y, just add to change...
 		
-		int x = 0;
-		int y = 0;
+		
 		
 		g.setColor(new Color(244, 206, 149));
-		g.fillOval(x+20, 550, 80, 80);
+		g.fillOval(20, 550, 80, 80);
 		g.fillRect(48, 625, 25, 10);
 		g.setColor(new Color(65, 20, 255));
 		g.fillRect(40, 635, 40, 65);
 		g.setColor(Color.BLACK);
 		g.fillOval(40, 575, 10, 10);//eyes
-		g.fillOval(60, 575, 10, 10);
+		g.fillOval(70, 575, 10, 10);
 		
 		
 	}
