032b4ed0d3e7c38fba8613e2b1b4942d03327211
ccarmona9800 <ccarmona9800@001LAB102010551.neisd.ad>
2015-04-08 15:36:03 -0500

    no animated man

--- Zoo-Map/src/AnimatedMan.java
+++ Zoo-Map/src/AnimatedMan.java
@@ -13,17 +13,7 @@ public class AnimatedMan {
 		//USE SPRITE instead in order to make animation smoother
 		
 		
-		g.setColor(new Color(244, 206, 149));
-		g.fillOval(20, 550, 80, 80);
-		g.fillRect(48, 625, 25, 10);
-		g.setColor(new Color(65, 20, 255));
-		g.fillRect(40, 635, 40, 65);
-		
-		//eyes
-		g.setColor(Color.BLACK);
-		g.fillOval(40, 575, 10, 10);
-		g.fillOval(70, 575, 10, 10);
-		
+
 		
 	}
 }
