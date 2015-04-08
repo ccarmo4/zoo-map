35a7ec69264dfb21d31c1ff25724c40515ec8c9e
brenda <brenda.mtz22@yahoo.com>
2015-04-08 15:31:07 -0500

    Drawem Buildings

--- Zoo-Map/src/Map.java
+++ Zoo-Map/src/Map.java
@@ -39,43 +39,8 @@ private AirHockey craft;
 		g.fillPolygon(path);
 	
 		
-		//Restrooms
-		g.setColor(new Color(128, 128, 128)); //building
-		g.fillRect(1200, 100, 220, 100);
-		g.setColor(new Color(145, 145, 145)); //divider
-		g.drawRect(1200, 110, 110, 90);		
-		g.drawRect(1310, 110, 110, 90);
-		g.setColor(Color.blue); //doors
-		g.fillRect(1220, 165, 20, 35);		
-		g.fillRect(1380, 165, 20, 35);
-		g.setColor(Color.white); //signs
-		g.setFont(new Font("RESTROOMS", Font.ITALIC, 12));
-		g.drawString("RESTROOMS", 1270, 110);
-		g.setFont(new Font("MENS", Font.BOLD, 10));
-		g.drawString("MENS", 1217, 160);		
-		g.drawString("WOMENS", 1367, 160);
-		
-		//Gift Shop
-		g.setColor(new Color(128, 64, 64)); //building
-		g.fillRect(200, 250, 140, 140); 
-		g.setColor(Color.gray); //doors
-		g.drawRect(247, 355, 20, 35); 
-		g.drawRect(246, 354, 22, 37);
-		g.drawRect(245, 353, 23, 38);
-		g.drawRect(267, 355, 20, 35);
-		g.drawRect(266, 354, 22, 37);
-		g.drawRect(265, 353, 24, 38);
-		g.setColor(Color.white); //sign
-		g.setFont(new Font("GIFT SHOP", Font.ITALIC, 14));
-		g.drawString("GIFT SHOP", 235, 275);
-		
-		//Restaurant (Burger place)
-		g.setColor(Color.yellow);
-		g.fillRect(600, 200, 140, 140); //building
-		g.setColor(Color.black);
-		g.setFont(new Font("JOE'S BURGER SHACK", Font.ITALIC, 12));
-		g.drawString("JOE'S BURGER SHACK", 605, 225);
-		
+		buildings.drawem(g);
+
 		//LINKING PART
 		AnimatedMan.drawMan(g);
 		
